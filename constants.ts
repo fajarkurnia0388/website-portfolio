fd5f77c52b588f60edb10ad79e2a41511b588fae
Fajar Kurnia <fk20160388@gmail.com>
2025-06-06 22:24:48 +0700

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -14,7 +14,7 @@ export const PROJECTS_DATA: Project[] = [
     title: 'AI Powered Code Reviewer',
     description: 'Aplikasi kalkulator sederhana yang dibuat menggunakan React dan TypeScript.',
     longDescription: 'An innovative tool that leverages advanced AI algorithms to perform comprehensive code reviews, identify potential bugs, and suggest improvements for better code quality and maintainability.',
-    imageUrl: {calculator},
+    imageUrl: calculator,
     githubLink: 'https://github.com/fajarkurnia0388/fajar-kurnia-calculator',
     liveLink: '#',
     tags: ['React', 'Vite', 'TypeScript', 'CSS', 'Other'],
