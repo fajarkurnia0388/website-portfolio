03dafd07ed878aad5171ada6714dc66cfb0e2953
Fajar Kurnia <fk20160388@gmail.com>
2025-06-07 14:19:55 +0700

    Update About.tsx

--- components/About.tsx
+++ components/About.tsx
@@ -23,13 +23,13 @@ const About: React.FC<AboutProps> = ({ id }) => {
         </div>
         <div className="text-lg text-gray-300 space-y-6" data-aos="fade-left" data-aos-delay="400">
           <p>
-            Hello! I'm **Fajar Kurnia**, a **Software Developer** with a deep passion for being a **Problem Solver and Digital Enthusiast**. My focus is on **Fullstack development**, and I possess expertise in **Python, JavaScript, and frameworks like Laravel**.
+            Hello! I'm Fajar Kurnia, a Software Developer with a deep passion for being a Problem Solver and Digital Enthusiast. My focus is on Fullstack development, and I possess expertise in Python, JavaScript, and frameworks like Laravel.
           </p>
           <p>
-            I'm deeply fascinated by digital technology and truly enjoy the process of **creating programs** that are both efficient and intuitive. My goal is to transform complex challenges into user-friendly solutions, continuously adding value through innovation.
+            I'm deeply fascinated by digital technology and truly enjoy the process of creating programs that are both efficient and intuitive. My goal is to transform complex challenges into user-friendly solutions, continuously adding value through innovation.
           </p>
           <p>
-            When I'm not immersed in coding, you can find me enjoying my hobbies like **reading comics, cycling, playing chess, and swimming**. These activities help me keep my mind fresh and maintain a healthy balance.
+            When I'm not immersed in coding, you can find me enjoying my hobbies like reading comics, cycling, playing chess, and swimming. These activities help me keep my mind fresh and maintain a healthy balance.
           </p>
           <div className="mt-8 flex justify-center md:justify-start space-x-4">
             <a href="https://github.com/fajarkurnia0388" target="_blank" rel="noopener noreferrer" className="text-sky-400 hover:text-sky-300 transition-colors duration-300 text-sm font-medium py-2 px-4 border border-sky-400 rounded-md hover:bg-sky-400 hover:text-gray-900">
